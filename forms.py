ca5a4a4043753484e905883880d09aaccd0b9ef0
T <papilozanowest@gmail.com>
2026-03-04 23:16:30 -0600

    Maestros Completos 04/03/2026

--- forms.py
+++ forms.py
@@ -1,29 +1,42 @@
-from wtforms import Form
+from wtforms import Form, StringField, DateField, IntegerField, EmailField,validators
 from flask_wtf import FlaskForm
  
-from wtforms import StringField, IntegerField
-from wtforms import EmailField
-from wtforms import validators
- 
-class UserForm(Form):
-    id = IntegerField('id', [
+class UserForm(FlaskForm):
+    id = IntegerField('id',[
         validators.number_range(min=1, max=20, message='valor no valido')
     ])
    
-    nombre = StringField('nombre', [
+    nombre = StringField('nombre',[
         validators.DataRequired(message='El nombre es requerido'),
-        validators.length(min=4, max=20, message='requiere min=4 max=20')
+        validators.length(min=4, max=20, message='requiere minimo 4, maximo 20')
     ])
    
-    apellidos = StringField('apellidos', [
-        validators.DataRequired(message='Los apellidos son requeridos')
+    apellidos = StringField('apellidos',[
+        validators.DataRequired(message='El apellido es requerido')
     ])
    
-    email = EmailField('correo', [
+    email = EmailField('correo',[
         validators.DataRequired(message='El correo es requerido'),
         validators.Email(message='Ingrese un correo valido')
     ])
-
-    telefono = StringField('telefono', [
+    
+    telefono = StringField('telefono',[
         validators.DataRequired(message='El telefono es requerido')
     ])
+    
+class UserForm2(FlaskForm):
+
+    id = IntegerField('id')
+
+    nombre = StringField('nombre')
+    apellidos = StringField('apellidos')
+    email = EmailField('correo')
+    telefono = EmailField('telefono')
+    
+    
+class MaestroForm(FlaskForm):
+    matricula = IntegerField('Matrícula', [validators.DataRequired(message='La Matricula es requerida')])
+    nombre = StringField('Nombre', [validators.DataRequired(message='El Nombre es requerido')])
+    apellidos = StringField('Apellidos', [validators.DataRequired(message='Los Apellidos es requerido')])
+    especialidad = StringField('Especialidad', [validators.DataRequired(message='La Especialidad es requerido')])
+    email = StringField('Email', [validators.DataRequired(), validators.Email(message='El correo es requerido')])
--- maestros/routes.py
+++ maestros/routes.py
@@ -1,5 +1,6 @@
+from models import Maestros
 from . import maestros
-from flask import render_template,request,redirect,url_for
+from flask import Flask, render_template, request,redirect,url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
@@ -11,14 +12,80 @@ from models import db
 from models import Alumnos, Maestros
 
 
-@maestros.route("/maestros",methods=['GET','POST'])
+@maestros.route("/maestros", methods=['GET','POST'])
 @maestros.route("/index")
 def index():
     create_form=forms.UserForm(request.form)
     maestros=Maestros.query.all()
-    return render_template("maestros/listadoMaes.html", form=create_form,maestros=maestros)
+    return render_template("maestros/listadoMaes.html",form=create_form,maestros=maestros)
+    
 
 
 @maestros.route('/perfil/<nombre>')
 def perfil(nombre):
     return f"Perfil de {nombre}"
+
+# CREAR
+@maestros.route("/maestros/crear", methods=['GET', 'POST'])
+def crear():
+    form = forms.MaestroForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        maestro = Maestros(
+            matricula=form.matricula.data,
+            nombre=form.nombre.data,
+            apellidos=form.apellidos.data,
+            especialidad=form.especialidad.data,
+            email=form.email.data
+        )
+        db.session.add(maestro)
+        db.session.commit()
+        return redirect(url_for('maestros.index'))
+
+    return render_template("maestros/crear.html", form=form)
+
+
+# DETALLES
+@maestros.route("/maestros/detalles/<int:matricula>")
+def detalles(matricula):
+    maestro = Maestros.query.get_or_404(matricula)
+
+    return render_template("maestros/detalles.html",
+                           matricula=maestro.matricula,
+                           nombre=maestro.nombre,
+                           apellidos=maestro.apellidos,
+                           especialidad=maestro.especialidad,
+                           email=maestro.email)
+
+
+# EDITAR
+@maestros.route("/maestros/editar/<int:matricula>", methods=['GET', 'POST'])
+def editar(matricula):
+    maestro = Maestros.query.get_or_404(matricula)
+    form = forms.MaestroForm(request.form, obj=maestro)
+
+    if request.method == 'POST' and form.validate():
+        maestro.matricula = form.matricula.data
+        maestro.nombre = form.nombre.data
+        maestro.apellidos = form.apellidos.data
+        maestro.especialidad = form.especialidad.data
+        maestro.email = form.email.data
+
+        db.session.commit()
+        return redirect(url_for('maestros.index'))
+
+    return render_template("maestros/editar.html", form=form)
+
+
+# ELIMINAR
+@maestros.route("/maestros/eliminar/<int:matricula>", methods=['GET', 'POST'])
+def eliminar(matricula):
+    maestro = Maestros.query.get_or_404(matricula)
+    form = forms.MaestroForm(request.form, obj=maestro)
+
+    if request.method == 'POST':
+        db.session.delete(maestro)
+        db.session.commit()
+        return redirect(url_for('maestros.index'))
+
+    return render_template("maestros/eliminar.html", form=form)
